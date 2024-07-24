e22326d31d01c1965702920f20c88d3b81789b4f
Robert Uleman <uleman@acm.org>
2024-07-23 18:05:33 -0700

    Added usage documentation to the docstring.

--- SensorDataIngest/__init__.py
+++ SensorDataIngest/__init__.py
@@ -5,4 +5,11 @@ Modules:
     layout.py               The Dash Mantine page elements and layout
     callbacks.py            The Dash callbacks that implement the page elements' behaviors
     helpers.py              Functions implementing actions that are independent of the Dash environment, called by callbacks
+
+Usage:
+    On the command line, with the directory containing this package as the current working directory, use this:
+    > python sensor_data_ingest.py
+
+    This starts a web server; using a browser, open localhost:8050 (or whichever port is listed in the response
+    to running the script).
 '''
