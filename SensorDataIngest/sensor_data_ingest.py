e4767f2b7136756a8b132b6fe843e7b020694b81
Robert Uleman <uleman@acm.org>
2024-08-08 12:09:03 -0700

    Added logging. Removed callback exception suppression because no longer relying on dynamic component creation.

--- SensorDataIngest/sensor_data_ingest.py
+++ SensorDataIngest/sensor_data_ingest.py
@@ -1,6 +1,8 @@
 #! ../.venv/bin/python
 '''Main module for the SensorDataIngest package for Pepperwood.'''
 
+import logging
+
 from dash import Dash, _dash_renderer
 
 import dash_mantine_components as dmc
@@ -20,11 +22,44 @@ stylesheets = [
     # 'https://unpkg.com/@mantine/nprogress@7/styles.css',
 ]
 
+# Set up logging.
+# Write logs to a subdirectory of the current directory (from where the script was started).
+# TODO: Set this up to be overridden by a command-line argument
+# If logging_dir does not exist, create it.
+# The current setup errors out if logging_dir exists but is not a directory (because then creating the log file fails).
+current_dir = Path('.')
+logging_dir = current_dir / 'logs'
+
+warn_logging_dir_created = False
+if not logging_dir.exists():
+    logging_dir.mkdir()
+    warn_logging_dir_created = True
+
+# module_name = Path(__file__).name           # Use this later, after renaming the source file
+module_name = 'ingest'
+
+logger = logging.getLogger(module_name.capitalize())
+file_handler     = logging.FileHandler(logging_dir / (module_name + '.log'))
+stream_handler   = logging.StreamHandler()
+file_formatter   = logging.Formatter('{asctime}|{name}|{levelname} {message}', style='{', datefmt='%Y-%m-%d %H:%M:%S')
+stream_formatter = logging.Formatter('{name}|{levelname} {message}', style='{')
+
+file_handler.setLevel('DEBUG')
+file_handler.setFormatter(file_formatter)
+stream_handler.setLevel('INFO')
+stream_handler.setFormatter(stream_formatter)
+logger.setLevel('DEBUG')
+logger.addHandler(file_handler)
+logger.addHandler(stream_handler)
+
+logger.info('Interactive ingest application started.')
+logger.info(f'Logging directory is {logging_dir.resolve()}.')
+if warn_logging_dir_created:
+    logger.warning('Logging directory did not yet exist and had to be created by this app.')
+
 # Standard creation of a Dash app with Dash Mantine Components.
-# Suppress callback exceptions because one of the callbacks refers to a component created by another callback
-# (not part of the static layout). This normally causes an "id is not part of the layout" error.
-app = Dash(external_stylesheets=stylesheets, suppress_callback_exceptions=True)
+app = Dash(external_stylesheets=stylesheets)
 app.layout = dmc.MantineProvider(layout)
 
 if __name__ == '__main__':
-    app.run(debug=False)
+    app.run(debug=True)
