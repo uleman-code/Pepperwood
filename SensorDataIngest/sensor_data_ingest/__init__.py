f4c2164075b22a5acd3f5803757f068c95b0fad8
Robert Uleman <uleman@acm.org>
2025-10-24 21:32:59 -0700

    Minor edits.

--- SensorDataIngest/sensor_data_ingest/__init__.py
+++ SensorDataIngest/sensor_data_ingest/__init__.py
@@ -1,7 +1,7 @@
 """This package implements an interactive data ingest app for Pepperwood Preserve environmental sensor data.
 
 Modules:
-    config.py       Configuration management
+    config.py       Configuration management and logging setup
     layout.py       The Dash Mantine page elements and layout
     callbacks.py    The Dash callbacks that implement the page elements' behaviors
     helpers.py      Functions implementing actions that are independent of the Dash
@@ -10,6 +10,3 @@ Modules:
 
 __version__: str       = '0.6'        # TODO: Need a proper versioning setup
 __all__:     list[str] = ['config', 'layout', 'callbacks', 'helpers']
-
-# from . import config as cfg
-# print(f'{module_name=}')
