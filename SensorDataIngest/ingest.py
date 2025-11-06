c2aa88406f6fc7117a9109a2774dab5dbb4b1475
Robert Uleman <uleman@acm.org>
2025-11-05 20:12:12 -0800

    Part of running in Plotly Cloud.

--- SensorDataIngest/ingest.py
+++ SensorDataIngest/ingest.py
@@ -10,45 +10,16 @@ from dash_extensions.enrich import (
     TriggerTransform,
 )
 
-
-# def main() -> None:
-    # """Configure the application and start the Dash process."""
-    # # Initialize the configuration module before importing any other modules from this project.
-    # module_name = Path(__file__).stem
-    # cfg.config_init(app_name=module_name)
-
-    # # Now that the config is initialized, we can import other modules.
-    # # This import has as a side-effect that it registers all the Dash callbacks.
-    # from sensor_data_ingest.callbacks import blueprint
-
-    # app: DashProxy = DashProxy(
-    #             blueprint=blueprint,
-    #             prevent_initial_callbacks=True,
-    #             title='Sensor Data Ingest',
-    #             update_title=None,                          # While rebuilding the page, don't
-    #                                                         # change tab title to "Updating..."
-    #             # background_callback_manager='diskcache',  # noqa: ERA001
-    #             transforms=[ServersideOutputTransform(), TriggerTransform()],
-    #            )
-    # server = app.server  # noqa: F841 # To make the Flask server available for deployment
-
-    # # NOTE: If the Dash app is run with debug=True, this main module is executed twice, resulting
-    # #       in duplicate logging output.
-    # #       This has to do with Flask and its support for automatic reloading upon any code changes.
-    # #       It can be suppressed, at the cost of losing that very convenient reloading behavior.
-    # #       The duplicate messages do not appear when debug=False.
-    # app.run(debug=cfg.config['application']['debug'])
-
-# main()
-"""Configure the application and start the Dash process."""
 # Initialize the configuration module before importing any other modules from this project.
 module_name = Path(__file__).stem
 cfg.config_init(app_name=module_name)
 
 # Now that the config is initialized, we can import other modules.
 # This import has as a side-effect that it registers all the Dash callbacks.
-from sensor_data_ingest.callbacks import blueprint
+from sensor_data_ingest.callbacks import blueprint  # noqa: E402
 
+# The following two statements must be at the module level, not inside a function.
+# Otherwise, the Dash app won't be properly discovered by deployment tools.
 app: DashProxy = DashProxy(
             blueprint=blueprint,
             prevent_initial_callbacks=True,
@@ -58,7 +29,7 @@ app: DashProxy = DashProxy(
             # background_callback_manager='diskcache',  # noqa: ERA001
             transforms=[ServersideOutputTransform(), TriggerTransform()],
             )
-server = app.server  # noqa: F841 # To make the Flask server available for deployment
+server = app.server  # noqa: F841
 
 if __name__ == '__main__':
     # NOTE: If the Dash app is run with debug=True, this main module is executed twice, resulting
@@ -66,6 +37,7 @@ if __name__ == '__main__':
     #       This has to do with Flask and its support for automatic reloading upon any code changes.
     #       It can be suppressed, at the cost of losing that very convenient reloading behavior.
     #       The duplicate messages do not appear when debug=False.
-    app.run(debug=cfg.config['application']['debug'])
-
-    logging.shutdown()
+    try:
+        app.run(debug=cfg.config['application']['debug'])
+    except KeyboardInterrupt:
+        logging.shutdown()
